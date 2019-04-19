95c3377d656890c150ef45beab1f021df2107cc2
deepshooter <avinashm720@gmail.com>
2019-04-19 15:05:09 +0530

    Adding camera preview

--- app/src/main/java/com/deepshooter/androidbarcodescanneroverlay/MainActivity.java
+++ app/src/main/java/com/deepshooter/androidbarcodescanneroverlay/MainActivity.java
@@ -1,15 +1,32 @@
 package com.deepshooter.androidbarcodescanneroverlay;
 
-import android.os.Bundle;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private CameraPreview mPreview;
+    private Camera mCamera;
+    private Context myContext;
+    private LinearLayout cameraPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        myContext = this;
+
+        mCamera = Camera.open();
+        mCamera.setDisplayOrientation(90);
+        cameraPreview = findViewById(R.id.cPreview);
+        mPreview = new CameraPreview(myContext, mCamera);
+        cameraPreview.addView(mPreview);
+
     }
 }
